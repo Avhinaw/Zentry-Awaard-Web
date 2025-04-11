b2c58bc29ede712c7979388c69cbeceb3aaf25dc
avhinaw <avhinaaw@gmail.com>
2025-04-11 11:18:21 +0530

    added text animations and rectify isuue

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,44 +1,64 @@
-import { useGSAP } from "@gsap/react"
-import gsap from 'gsap'
-import { ScrollTrigger } from "gsap/all"
-gsap.registerPlugin(ScrollTrigger)
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+import AnimatedTitle from "./AnimatedTitle";
+
+// import AnimatedTitle from "./AnimatedTitle";
+
+gsap.registerPlugin(ScrollTrigger);
+
 const About = () => {
-    useGSAP(() => {
-        const clipAnimation = gsap.timeline({
-            scrollTrigger: {
-                trigger: '#clip',
-                start: 'center center',
-                end: '+800 center',
-                scrub: 0.5,
-                pin: true,
-                pinSpacing: true
-            }
-        })
-        clipAnimation.to('.mask-clip-path', {
-            width: '100vw',
-            height: '100vh',
-            borderRadius: 0
-        })
-    })
+  useGSAP(() => {
+    const clipAnimation = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#clip",
+        start: "center center",
+        end: "+=900 center",
+        scrub: 0.5,
+        pin: true,
+        pinSpacing: true,
+      },
+    });
+
+    clipAnimation.to(".mask-clip-path", {
+      width: "100vw",
+      height: "100vh",
+      borderRadius: 0,
+    });
+  });
+
   return (
     <div id="about" className="min-h-screen w-screen">
-        <div className="relative mb-8 mt-36 flex-col items-center gap-5">
-            <h2 className="font-general text-sm uppercase md:text-[10px]">welcome to zentry</h2>
-            <div className="mt-5 text-center text-4xl uppercase leading-0.8 md:text-[6rem]">
-                disc<b>o</b>ver the world's <br /> largest shared <b>a</b>dventure,
-            </div>
-            <div className="about-subtext">
-                <p>The Metagame begains-your life, now an epic MMORPG</p>
-                <p>Zentry is the unified play layer driving attention and contribution through cross-world AI gamification.</p>
-            </div>
-        </div>
-        <div className="h-dvh w-screen" id="clip">
-            <div className="mask-clip-patch about-image">
-            <img src="images/about.webp" alt="background" className="absolute left-0 top-0 size-full object-cover"/>
-            </div>
-        </div>
-    </div>
-  )
-}
+      <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+        <p className="font-general text-sm uppercase md:text-[10px]">
+          Welcome to Zentry
+        </p>
 
-export default About
+        <AnimatedTitle
+          title="Disc<b>o</b>ver the world's <br /> largest shared <b>a</b>dventure"
+          containerClass="mt-5 !text-black text-center"
+        />
+
+        <div className="text-center">
+          <p>The Game of Games begins—your life, now an epic MMORPG</p>
+          <p className="text-gray-500 text-center">
+            Zentry unites every player from countless games and platforms, both
+            digital and physical, into a unified Play Economy
+          </p>
+        </div>
+      </div>
+
+      <div className="h-dvh w-screen" id="clip">
+        <div className="mask-clip-path about-image">
+          <img
+            src="images/about.webp"
+            alt="Background"
+            className="absolute left-0 top-0 size-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
